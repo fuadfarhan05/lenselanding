70ebf36a8d802e68ae59ea64688cf57c3fdcdb9e
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-08-20 20:47:31 -0400

    landingform

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example placeholder script
+document.querySelector(".cta-btn").addEventListener("click", () => {
+  alert("Hi! you are being redirecting to the waitlist form. Press ok to redirect.");
+});
