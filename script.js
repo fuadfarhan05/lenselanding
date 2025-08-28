700ae7b43ffdb084823073102c4cb2c8e5f35a6a
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-08-27 21:26:39 -0400

    future

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Example placeholder script
-document.querySelector(".cta-btn").addEventListener("click", () => {
-  alert("Hi! you are being redirecting to the waitlist form. Press ok to redirect.");
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const slider = document.querySelector('.slider-container');
